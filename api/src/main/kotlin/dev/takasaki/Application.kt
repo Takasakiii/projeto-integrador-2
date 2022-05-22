182880d6579a6d057dc8c1e4bbd44f54bb4cf239
Takasakiii <lucasmc2709@live.com>
2022-05-22 16:05:32 -0300

    commit inicial

---
+++ api/src/main/kotlin/dev/takasaki/Application.kt
@@ -0,0 +1,15 @@
+package dev.takasaki
+
+import io.ktor.server.application.*
+import dev.takasaki.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+    configureSerialization()
+    configureHTTP()
+    configureSecurity()
+}
