bd10178ef34e65a722a7b2f96ae9f79d6e2e008b
Takasakiii <lucasmc2709@live.com>
2022-05-24 15:11:18 -0300

    adicionado: Ação de registro de usuario na database

--- api/src/main/kotlin/dev/takasaki/database/Users.kt
+++ api/src/main/kotlin/dev/takasaki/database/Users.kt
@@ -1,6 +1,16 @@
 package dev.takasaki.database
 
+import com.toxicbakery.bcrypt.Bcrypt
+import cool.graph.cuid.Cuid
+import dev.takasaki.dtos.SecureUser
+import dev.takasaki.dtos.User
+import dev.takasaki.exceptions.database.DuplicateRegisterException
+import org.jetbrains.exposed.exceptions.ExposedSQLException
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.sql.SQLIntegrityConstraintViolationException
+import java.util.*
 
 object Users : Table() {
     val id = varchar("id", 32)
@@ -12,5 +22,32 @@ object Users : Table() {
     val userType = enumeration<UserType>("user_type")
 
     override val primaryKey = PrimaryKey(id)
+
+    fun register(user: User): SecureUser {
+        try {
+            val idGen = Cuid.createCuid()
+            val passwordHashRaw = Bcrypt.hash(user.password, 10)
+            val passwordHash = Base64.getEncoder().encodeToString(passwordHashRaw)
+
+            transaction {
+                Users.insert {
+                    it[id] = idGen
+                    it[name] = user.name
+                    it[email] = user.email
+                    it[password] = passwordHash
+                    it[surname] = user.surname
+                    it[phone] = user.phone
+                    it[userType] = user.userType
+                }
+            }
+
+            return SecureUser(idGen, user.name, user.surname, user.email, user.phone, user.userType)
+        } catch (e: ExposedSQLException) {
+            if (e.cause is SQLIntegrityConstraintViolationException) {
+                throw DuplicateRegisterException("User with email ${user.email} already exists")
+            }
+            throw e
+        }
+    }
 }
 
