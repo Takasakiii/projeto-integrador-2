061d835a27aafafac6ea3a4c74de5cd35ea3665b
Takasakiii <lucasmc2709@live.com>
2022-05-24 15:12:32 -0300

    adicionado: Auth.register controller

--- api/src/main/kotlin/dev/takasaki/controllers/Auth.kt
+++ api/src/main/kotlin/dev/takasaki/controllers/Auth.kt
@@ -1,11 +1,20 @@
 package dev.takasaki.controllers
 
+import dev.takasaki.database.Users
+import dev.takasaki.dtos.User
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.authRouter() {
     route("/auth") {
         post("/register") {
-
+            val user = call.receive<User>()
+            user.validateItems()
+            val registeredUser = Users.register(user)
+            call.respond(HttpStatusCode.Created, registeredUser)
         }
     }
 }
