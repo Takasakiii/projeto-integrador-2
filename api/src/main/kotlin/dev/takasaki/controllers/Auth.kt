01d9a8b5fd817df5900198e26b25d37167278d31
Takasakiii <lucasmc2709@live.com>
2022-05-24 18:40:14 -0300

    adicionado: Lógica do Login no controller

--- api/src/main/kotlin/dev/takasaki/controllers/Auth.kt
+++ api/src/main/kotlin/dev/takasaki/controllers/Auth.kt
@@ -1,12 +1,17 @@
 package dev.takasaki.controllers
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import dev.takasaki.database.Users
+import dev.takasaki.dtos.Login
+import dev.takasaki.dtos.Token
 import dev.takasaki.dtos.User
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.util.*
 
 fun Route.authRouter() {
     route("/auth") {
@@ -15,6 +20,28 @@ fun Route.authRouter() {
             user.validateItems()
             val registeredUser = Users.register(user)
             call.respond(HttpStatusCode.Created, registeredUser)
+        }
+        post("/login") {
+            val loginData = call.receive<Login>()
+            loginData.validateItems()
+            val userData = Users.login(loginData)
+
+            val config = this@authRouter.environment?.config ?: throw java.lang.Exception("Environment config is null")
+
+            val secret = config.property("jwt.secret").getString()
+            val issuer = config.property("jwt.domain").getString()
+            val audience = config.property("jwt.audience").getString()
+
+            val token = JWT.create()
+                .withAudience(audience)
+                .withIssuer(issuer)
+                .withClaim("id", userData.id)
+                .withExpiresAt(Date(System.currentTimeMillis() + 60000))
+                .sign(Algorithm.HMAC256(secret))
+
+            val tokenResponse = Token(userData, token)
+
+            call.respond(HttpStatusCode.OK, tokenResponse)
         }
     }
 }
