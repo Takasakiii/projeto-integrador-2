820880bf6e052239b76f328743abef99799caf33
Takasakiii <lucasmc2709@live.com>
2022-05-24 15:12:03 -0300

    configurado: StatusPage para gerir as exceptions previstas

--- api/src/main/kotlin/dev/takasaki/plugins/Routing.kt
+++ api/src/main/kotlin/dev/takasaki/plugins/Routing.kt
@@ -1,19 +1,32 @@
 package dev.takasaki.plugins
 
 import dev.takasaki.controllers.authRouter
+import dev.takasaki.exceptions.database.DuplicateRegisterException
+import dev.takasaki.extensions.getValidationErrors
 import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
+import org.valiktor.ConstraintViolationException
+import dev.takasaki.dtos.Error as ErrorDTO
 
 fun Application.configureRouting() {
     install(StatusPages) {
-        exception<AuthenticationException> { call, cause ->
-            call.respond(HttpStatusCode.Unauthorized)
+        exception<DuplicateRegisterException> {call, cause ->
+            call.respond(HttpStatusCode.Conflict,
+                ErrorDTO(message = cause.message ?: "Duplicate register")
+            )
         }
-        exception<AuthorizationException> { call, cause ->
-            call.respond(HttpStatusCode.Forbidden)
+        exception<ConstraintViolationException> {call, cause ->
+            call.respond(HttpStatusCode.UnprocessableEntity,
+                ErrorDTO(message = cause.getValidationErrors())
+            )
+        }
+        exception<Exception> {call, cause ->
+            call.respond(HttpStatusCode.InternalServerError,
+                ErrorDTO(message = cause.message ?: "Internal server error")
+            )
         }
     
     }
@@ -22,5 +35,3 @@ fun Application.configureRouting() {
         authRouter()
     }
 }
-class AuthenticationException : RuntimeException()
-class AuthorizationException : RuntimeException()
