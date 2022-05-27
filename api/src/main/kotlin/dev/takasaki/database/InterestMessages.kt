6bf855ac36a260f9de0d6ea8d4a871b27799c8a3
Takasakiii <lucasmc2709@live.com>
2022-05-27 00:56:02 -0300

    Editando: InterestMessages table para conter algumas index extras, createAt e adicionado ação de adicionar

--- api/src/main/kotlin/dev/takasaki/database/InterestMessages.kt
+++ api/src/main/kotlin/dev/takasaki/database/InterestMessages.kt
@@ -1,12 +1,47 @@
 package dev.takasaki.database
 
+import cool.graph.cuid.Cuid
+import dev.takasaki.dtos.InterestMessage
+import dev.takasaki.dtos.InterestMessagesResponse
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
 
 object InterestMessages: Table() {
     private val id = varchar("id", 32)
-    private val item = reference("item", Items.id)
-    private val userSend = reference("user_send", Users.id)
-    private val userReceive = reference("user_receive", Users.id)
+    private val item = reference("item", Items.id).index()
+    private val userSend = reference("user_send", Users.id).index()
+    private val userReceive = reference("user_receive", Users.id).index()
     private val message = text("message")
-    private val read = bool("read").index()
+    private val createdAt = long("created_at")
+    private val read = bool("read").index().default(false)
+
+    fun create(message: InterestMessage, messageAuthor: String): InterestMessagesResponse {
+        val idGen = Cuid.createCuid()
+        val now = System.currentTimeMillis()
+
+        val item = transaction {
+            val locatedItem = Items.get(message.item)
+
+            InterestMessages.insert {
+                it[id] = idGen
+                it[item] = locatedItem.id
+                it[userSend] = messageAuthor
+                it[userReceive] = locatedItem.owner
+                it[this.message] = message.message
+                it[createdAt] = now
+            }
+
+            locatedItem
+        }
+
+        return InterestMessagesResponse(
+            idGen,
+            item.id,
+            messageAuthor,
+            item.owner,
+            message.message,
+            now,
+        )
+    }
 }
