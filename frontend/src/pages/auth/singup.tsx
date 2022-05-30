f2a7544097e4e3c35e5dc4486d1acde5efe0f955
Takasakiii <lucasmc2709@live.com>
2022-05-30 16:29:51 -0300

    substitundo: form structure pelo component
    adicionando: suporte a mensagem de sucesso

--- frontend/src/pages/auth/singup.tsx
+++ frontend/src/pages/auth/singup.tsx
@@ -1,4 +1,3 @@
-import { css } from "@emotion/react";
 import { NextPage } from "next";
 import Field from "../../components/Field";
 import MaskedField from "../../components/MaskedField";
@@ -7,88 +6,89 @@ import { useForm, Controller } from "react-hook-form";
 import { UserRegister } from "../../api";
 import doacaoApi from "../../api";
 import { useRef, useState } from "react";
-import Notification, { NotificationRef } from "../../components/Notification";
-
-const container = css({
-  display: "flex",
-  justifyContent: "center",
-  paddingTop: "4rem",
-});
-
-const box = css({
-  width: "clamp(300px, 30%, 600px)",
-});
+import Notification, {
+  NotificationProps,
+  NotificationRef,
+} from "../../components/Notification";
+import Form from "../../components/Form";
 
 const SingUpPage: NextPage = () => {
   const form = useForm<UserRegister>();
   const notificationRef = useRef<NotificationRef>(null);
-  const [notificationContent, setNotificationContent] = useState("");
+  const [notificationContent, setNotificationContent] =
+    useState<NotificationProps>({
+      children: "",
+      color: "danger",
+    });
 
   function handleSubmit(data: UserRegister) {
-    doacaoApi.registerUser(data).catch((err) => {
-      setNotificationContent(err.message);
-      notificationRef.current?.show();
-    });
+    doacaoApi
+      .registerUser(data)
+      .catch((err) => {
+        setNotificationContent({
+          children: err.message,
+          color: "danger",
+        });
+        notificationRef.current?.show();
+      })
+      .then(() => {
+        setNotificationContent({
+          children: "Usuário cadastrado com sucesso!",
+          color: "success",
+        });
+      });
   }
 
   return (
-    <div css={container}>
-      <form
-        className="box"
-        css={box}
-        onSubmit={form.handleSubmit(handleSubmit)}
-      >
-        <Notification ref={notificationRef} color="danger">
-          {notificationContent}
-        </Notification>
+    <Form onSubmit={form.handleSubmit(handleSubmit)}>
+      <Notification {...notificationContent} ref={notificationRef} />
 
-        <h1 className="title">Registre-se</h1>
-        <Field
-          label="Nome"
-          placeholder="João"
-          required
-          minLength={3}
-          maxLength={255}
-          {...form.register("name")}
-        />
-        <Field
-          label="Sobrenome"
-          placeholder="Silva Souza"
-          required
-          minLength={3}
-          maxLength={255}
-          {...form.register("surname")}
-        />
-        <Field
-          label="Email"
-          type="email"
-          required
-          placeholder="joaosilva@gmail.com"
-          {...form.register("email")}
-        />
-        <Field
-          label="Senha"
-          type="password"
-          required
-          minLength={8}
-          placeholder="●●●●●●●●"
-          {...form.register("password")}
-        />
-        <Controller
-          name="phone"
-          control={form.control}
-          render={({ field }) => (
-            <MaskedField
-              mask={formatTellOrCell}
-              label="Telefone"
-              placeholder="(00) 00000-0000"
-              {...field}
-            />
-          )}
-        />
-        <button className="button is-primary">Registrar</button>
-      </form>
-    </div>
+      <h1 className="title">Registre-se</h1>
+      <Field
+        label="Nome"
+        placeholder="João"
+        required
+        minLength={3}
+        maxLength={255}
+        {...form.register("name")}
+      />
+      <Field
+        label="Sobrenome"
+        placeholder="Silva Souza"
+        required
+        minLength={3}
+        maxLength={255}
+        {...form.register("surname")}
+      />
+      <Field
+        label="Email"
+        type="email"
+        required
+        placeholder="joaosilva@gmail.com"
+        {...form.register("email")}
+      />
+      <Field
+        label="Senha"
+        type="password"
+        required
+        minLength={8}
+        placeholder="●●●●●●●●"
+        {...form.register("password")}
+      />
+      <Controller
+        name="phone"
+        control={form.control}
+        render={({ field }) => (
+          <MaskedField
+            mask={formatTellOrCell}
+            label="Telefone"
+            placeholder="(00) 00000-0000"
+            {...field}
+          />
+        )}
+      />
+      <button className="button is-primary">Registrar</button>
+    </Form>
   );
 };
 
