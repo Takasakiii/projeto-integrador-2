3a6116c005eb858a69b999276f9afd693d3820cc
Takasakiii <lucasmc2709@live.com>
2022-05-30 16:46:31 -0300

    finalizando: login page

--- frontend/src/pages/auth/login.tsx
+++ frontend/src/pages/auth/login.tsx
@@ -1,6 +1,8 @@
 import { NextPage } from "next";
+import { useRouter } from "next/router";
 import { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
 import doacaoApi, { Login } from "../../api";
 import Field from "../../components/Field";
 import Form from "../../components/Form";
@@ -8,6 +10,7 @@ import Notification, {
   NotificationProps,
   NotificationRef,
 } from "../../components/Notification";
+import { login } from "../../storage/Auth.slice";
 
 const LoginPage: NextPage = () => {
   const notificationRef = useRef<NotificationRef>(null);
@@ -17,19 +20,22 @@ const LoginPage: NextPage = () => {
       color: "danger",
     });
   const form = useForm<Login>();
+  const dispatch = useDispatch();
+  const router = useRouter();
 
   function handleSubmit(data: Login) {
     doacaoApi
       .login(data)
+      .then((data) => {
+        dispatch(login(data));
+        router.push("/");
+      })
       .catch((err) => {
         setNotificationContent({
           children: err.message,
           color: "danger",
         });
         notificationRef.current?.show();
-      })
-      .then((data) => {
-        console.log(data);
       });
   }
 
