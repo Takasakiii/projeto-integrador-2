299f9ff569575ef3b1612f15096017ceb6933bc3
Takasakiii <lucasmc2709@live.com>
2022-05-30 12:02:11 -0300

    adicionado: margin entre navbar e content

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,12 +2,20 @@ import "bulma/css/bulma.min.css";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
+import { css } from "@emotion/react";
+
+const contentMargin = css({
+  marginTop: "4rem",
+});
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <main>
       <Navbar />
-      <Component {...pageProps} />;
+      <section id="main" css={contentMargin}>
+        <Component {...pageProps} />
+      </section>
     </main>
   );
 }
