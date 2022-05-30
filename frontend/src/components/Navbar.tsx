b742b187b725743f0aaf807916e16b0079b39eda
Takasakiii <lucasmc2709@live.com>
2022-05-30 09:32:03 -0300

    adicionado: base do frontend

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,72 @@
+import { css } from "@emotion/react";
+import Link from "next/link";
+import { useState } from "react";
+import classNames from "classnames";
+
+const titleMarca = css({
+  fontFamily: "var(--font-marca)",
+  fontSize: "1.4rem",
+});
+
+const Navbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <nav
+      className="navbar is-fixed-top"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="navbar-brand">
+        <Link href="/">
+          <a className="navbar-item">
+            <h1 css={titleMarca}>Donations</h1>
+          </a>
+        </Link>
+
+        <a
+          role="button"
+          className={classNames("navbar-burger", {
+            "is-active": isOpen,
+          })}
+          onClick={handleToggle}
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div
+        id="navbarBasicExample"
+        className={classNames("navbar-menu", {
+          "is-active": isOpen,
+        })}
+      >
+        {/* <div className="navbar-start">
+          <a className="navbar-item">Pagina Inicial</a>
+        </div> */}
+
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <a className="button is-primary">
+                <strong>Cadastrar-se</strong>
+              </a>
+              <a className="button is-light">Logar-se</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import "bulma/css/bulma.min.css";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Navbar from "../components/Navbar";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <main>
+      <Navbar />
+      <Component {...pageProps} />;
+    </main>
+  );
+}
+
+export default MyApp;
