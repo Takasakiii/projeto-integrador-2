b2bc6d8a8a3398f36379ef7885e76a9048ade80d
Takasakiii <lucasmc2709@live.com>
2022-05-30 16:41:51 -0300

    adicionado: provider no _app.tsx

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,11 +3,12 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
 import { css } from "@emotion/react";
+import { Provider } from "react-redux";
+import store from "../storage";
 
 const contentMargin = css({
   marginTop: "4rem",
 });
-
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -20,4 +21,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-export default MyApp;
+function Index(props: AppProps) {
+  return (
+    <Provider store={store}>
+      <MyApp {...props} />
+    </Provider>
+  );
+}
+
+export default Index;
